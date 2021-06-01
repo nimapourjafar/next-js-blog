955b4f8494a3a70a1a43af68281662f30c1abc40
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-01 00:00:53 -0400

    styles

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,16 @@ import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/about"><a>About</a></Link></li>
-        </ul>
-      </nav>
+    <div className="mx-auto w-1/2 my-9">
+      <header>
+        <h1 className="text-6xl font-bold text-center">Skinny Origins</h1>
+        <nav>
+          <ul className="flex flex-row space-x-9 justify-center m-6 text-lg">
+            <li><Link href="/"><a>Home</a></Link></li>
+            <li><Link href="/about"><a>About</a></Link></li>
+          </ul>
+        </nav>
+      </header>
       <Component {...pageProps}/>
     </div>
   )
